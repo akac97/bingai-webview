c15f50ac74f9456208dcaee1080aac29d5368790
akac97 <142359207+akac97@users.noreply.github.com>
2024-03-09 22:51:05 +0100

    Update MainActivity.kt

--- app/src/main/java/com/webviewtemplate/webviewtemplate/MainActivity.kt
+++ app/src/main/java/com/webviewtemplate/webviewtemplate/MainActivity.kt
@@ -3,18 +3,12 @@ package com.webviewtemplate.webviewtemplate
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Build
-import android.os.Bundle
 import android.webkit.WebView
 import android.window.OnBackInvokedDispatcher
 import com.webviewtemplate.webviewtemplate.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
-    // you can make offline application with local file
-    //private val applicationUrl = "file:///android_asset/index.html"
-    //or you can load url
-    //private val applicationUrl ="https://www.programmerbee.com/"
     private val applicationUrl = "https://www.bing.com/copilot"
-    
     private lateinit var binding: ActivityMainBinding
     private lateinit var webView: WebView
 
@@ -24,7 +18,6 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         webView = binding.webView
-
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             onBackInvokedDispatcher.registerOnBackInvokedCallback(
@@ -38,12 +31,13 @@ class MainActivity : Activity() {
             }
         }
 
-
         webView.settings.domStorageEnabled = true
         webView.settings.javaScriptEnabled = true
 
+        // Set user agent to mobile Microsoft Edge
+        val edgeUserAgent = "Mozilla/5.0 (Linux; Android 10; HD1913) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.144 Mobile Safari/537.36 EdgA/120.0.2210.64"
+        webView.settings.userAgentString = edgeUserAgent
 
         webView.loadUrl(applicationUrl)
     }
-
 }
